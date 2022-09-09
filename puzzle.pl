5b9b0c2676f2d1f9b077f077931d2d126b4fce3f
Víctor Cuaya <vcuaya@outlook.com>
2022-09-09 04:50:55 -0500

    Actualización del código en base a lo proporcionado por el profesor para la regla, pon(), coloca(), sucesores() y auxiliar()

--- puzzle.pl
+++ puzzle.pl
@@ -12,12 +12,13 @@
     Regla Coordenada
     coordenada(Puzzle,Elemento,[X,Y])
     Regresa las coordenadas de un elemento
+    coordenada([[2,8,3],[1,6,4],[7,0,5]], 0, [3,2]).
 ======================================================================================
 */
 % Caso base
 coordenada([R1|_], Elemento, [1,Y]):-
     member(Elemento, R1),
-    traeY(R1, Elemento, Y).
+    traeElementoY(R1, Y, Elemento).
 
 % Caso general
 coordenada([_|Resto], Elemento, [X, Y]):-
@@ -26,58 +27,45 @@ coordenada([_|Resto], Elemento, [X, Y]):-
 
 /*
 ======================================================================================
-    Regla Trae Coordenada Y
-    traeY(Renglon, Elemento, CoordenadaY).
-    Revisa el Renglon actual y regresa la coordenada de Y
-======================================================================================
-*/
-% Caso base
-traeY([E1|_], E1, 1).
-
-% Caso general
-traeY([_|Resto], Elemento, Y):-
-    traeY(Resto, Elemento, NewY),
-    Y is NewY + 1.
-
-/*
-======================================================================================
     Regla Suma
-    suma(Movimientos, Coordenada, Lista).
+    suma(Reglas de Movimiento, Coordenada, Posibles Movimientos).
     Regresa las coordenadas adyacentes de la posición dada
+    suma([[1,0],[0,1],[-1,0],[0,-1]], [3,2], Posibles Movimientos).
 ======================================================================================
 */
 % Caso base
 suma([], _, []).
 
 % Caso general
-suma([[X,Y]|Resto], [X1,Y1], [[ResultadoX,ResultadoY]|RestoRes]):-
+suma([[X,Y]|RestoReglas], [X1,Y1], [[ResultadoX,ResultadoY]|RestoResultados]):-
     ResultadoX is X + X1,
     ResultadoY is Y + Y1,
-    suma(Resto, [X1,Y1], RestoRes).
+    suma(RestoReglas, [X1,Y1], RestoResultados).
 
 /*
 ======================================================================================
     Regla Valida movimientos
-    validaMov(Movimientos,Validas).
+    valida(Movimientos,Validos).
     Regresa la lista de movimientos válidos
+    valida([[4, 2], [3, 3], [2, 2], [3, 1]],Validos).
 ======================================================================================
 */
 % Caso base
-validaMov([],[]).
+valida([],[]).
 
 % Casos generales
-validaMov([[X,Y]|RestoMov],[[X,Y]|RestoVal]):-
+valida([[X,Y]|RestoMovimientos],[[X,Y]|RestoValidos]):-
     ok(X,Y),
-    validaMov(RestoMov,RestoVal).
+    valida(RestoMovimientos,RestoValidos).
 
-validaMov([_|RestoMov],RestoVal):-
-    validaMov(RestoMov,RestoVal).
+valida([_|RestoMovimientos],RestoValidos):-
+    valida(RestoMovimientos,RestoValidos).
 
 /*
 ======================================================================================
     Regla Ok
     ok(X,Y)
-    Revisa que la coordenada dad no rebase el tamaño del tablero
+    Revisa que la coordenada dada no rebase el tamaño del tablero
 ======================================================================================
 */
 ok(X,Y):-
@@ -91,6 +79,7 @@ ok(X,Y):-
     Regla Trae Elemento
     traeElemento(Renglon,Coordenada,Elemento)
     Regla que trae un elemento dada una coordenada
+    traeElemento([[2,8,3],[1,6,4],[7,0,5]], [2,2], Elemento).
 ======================================================================================
 */
 
@@ -107,7 +96,8 @@ traeElemento([_|Resto],[X,Y],Elemento):-
 ======================================================================================
     Regla Trae Elemento Y
     traeElemento(Renglon,Coordenada Y,Elemento)
-    Regla que trae un elemento dada una coordenada Y
+    Revisa el Renglon actual y regresa la coordenada de Y
+    traeElementoY([1,6,4], 3, Elemento).
 ======================================================================================
 */
 
@@ -122,25 +112,58 @@ traeElementoY([_|Resto],Y,Elemento):-
 /*
 ======================================================================================
     Regla Pon
-    
-    
+    pon(Puzzle, Coordenada, Elemento, Resultado)
+    pon([[2,8,3],[1,6,4],[7,0,5]], [3,2], 0, Resultado). -> Revisar desde aquí
 ======================================================================================
 */
 % Caso Base
-pon([R1|Resto],Elemento,[1,Y],[Nreng|Resto]):-
+pon([R1|Resto],Elemento,[1,Y],[NuevoRenglon|Resto]):-
     coloca(R1,Elemento,Y,Nreng).
 
 %Caso General
-pon([R1|Resto],Elem,[X,Y],[R1|RestoM]):-
-    Xn is X-1,
-    pon(Resto,Elem,[Xn,Y],RestoM).
+pon([R1|Resto],Elemento,[X,Y],[R1|RestoM]):-
+    NewX is X-1,
+    pon(Resto,Elemento,[NewX,Y],RestoM).
 
+/*
+======================================================================================
+    Regla Coloca
+    coloca(Renglon, Elemento, Cordenada Y, NuevoElemento)
+    traeElementoY([[3, 3], [2, 2], [3, 1]])
+======================================================================================
+*/
 % Caso Base
-coloca([_|Resto],Elem,1,[Elem|Resto]).
+coloca([_|Resto],Elemento,1,[Elemento|Resto]).
 
 % Caso General
-coloca([P|Resto],Elem,Y,[P|RestoNr]):-Yn is Y-1,coloca(Resto,Elem,Yn,RestoNr).
+coloca([P|Resto],Elemento,Y,[P|RestoNr]):-
+    NewY is Y-1,
+    coloca(Resto,Elemento,NewY,RestoNr).
 
-% sucesores(Puzzle, MovimientosValidos, NuevosPuzzles)
-sucesores(Puzzle, [], NuevosPuzzles).
-sucesores(Puzzle, [P|Resto], NuevosPuzzles):-traeElemento(Puzzle, P, Elemento), coordenada(), pon(Puzzle, Elemento, X, Y, Resultado).
+/*
+======================================================================================
+    Regla Sucesores
+    sucesores(Puzzle, Sucesores)
+
+======================================================================================
+*/
+sucesores(Puzzle, Sucesores):-
+    coordenada(Puzzle, 0, Coordenada),
+    suma([[1,0],[0,1],[0,-1],[-1,0]],Coordenada, Movimientos),
+    valida(Movimientos,Validos), auxiliar(Puzzle, Validos, Sucesores).
+
+/*
+======================================================================================
+    Regla 
+    sucesores(Puzzle, Sucesores)
+
+======================================================================================
+*/
+% Caso base
+auxiliar(_,[],[]).
+auxiliar(Puzzle, [[X,Y]|RestoValidos], [P|RestoPuzzle]):-
+    traeElemento(Puzzle,[X,Y],Elemento),
+    pon(Puzzle,[X,Y],0,NP),
+    coordenada(Puzzle,0,Coordenada),
+    pon(NP,Coordenada,Elemento,PF),
+    auxiliar(Puzzle, RestoValidos, RestoPuzzle).
